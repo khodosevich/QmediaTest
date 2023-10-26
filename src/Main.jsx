74022690469c265bb7f1b5db2e16d651b947f96e
Matvey <hodos.952795@gmail.com>
2023-10-26 12:33:59 +0300

    init react app

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import QuizComponent from "./components/QuizComponent";
+
+
+const Main = () => {
+    return (
+        <div>
+            Qmedia
+            <QuizComponent/>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/QuizComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from "./quiz.module.css"
+const QuizComponent = () => {
+    return (
+        <div className={classes.hello}>
+            Quiz
+        </div>
+    );
+};
+
+export default QuizComponent;
