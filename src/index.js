53ff1ac5a4e587d899e5effcd7ead4d0f0b416da
Matvey <hodos.952795@gmail.com>
2023-10-27 11:38:26 +0300

    change browser router on hash router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Main from "./Main";
 
 import "./style/reset.css"
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Main />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
